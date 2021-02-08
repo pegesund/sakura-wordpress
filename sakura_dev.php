c7b54bf9f70b58b381fb3bcf15758a4cdcd18e0e
Jingtao Xu <jingtaozf@gmail.com>
2021-02-08 11:06:48 +0800

    add a plugin for local development.

---
+++ sakura_dev.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package The development plugin for Sakura Network.
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Sakura network internal development
+Plugin URI: https://sakura.eco/
+Description: This is just a plugin for development use only, to make us local development easy.
+Author: Sakura.eco
+Version: 1.0.0
+Author URI: https://sakura.eco/
+*/
+
+add_filter( 'http_request_args', function ( $args ) {
+
+    $args['reject_unsafe_urls'] = false;
+
+    return $args;
+}, 999 );
+
+function read_sakura_server_for_dev ($arg) {
+  return file_get_contents('/srv/sakura_address.txt');
+}
+add_filter( 'sakura_update_server_address', 'read_sakura_server_for_dev', 999 );
