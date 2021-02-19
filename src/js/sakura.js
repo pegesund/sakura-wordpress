e4f34cc878da8634977205e7d34cc51bf5cd7a7c
Jingtao Xu <jingtaozf@gmail.com>
2021-02-19 12:35:49 +0800

    remove log message.

--- src/js/sakura.js
+++ src/js/sakura.js
@@ -2,24 +2,24 @@ iFrameResize({
     log                     : false,                  // Disable console logging
     maxHeight: 400,
     resizedCallback         : function(messageData){ // Callback fn when resize is received
-	console.log(
-	    '<b>Frame ID:</b> '    + messageData.iframe.id +
-	    ' <b>Height:</b> '     + messageData.height +
-	    ' <b>Width:</b> '      + messageData.width +
-	    ' <b>Event type:</b> ' + messageData.type
-	);
+	// console.log(
+	//     '<b>Frame ID:</b> '    + messageData.iframe.id +
+	//     ' <b>Height:</b> '     + messageData.height +
+	//     ' <b>Width:</b> '      + messageData.width +
+	//     ' <b>Event type:</b> ' + messageData.type
+	// );
     },
     messageCallback         : function(messageData){ // Callback fn when message is received
-	console.log(
-	    '<b>Frame ID:</b> '    + messageData.iframe.id +
-	    ' <b>Message:</b> '    + messageData.message
-	);
-	alert(messageData.message);
+	// console.log(
+	//     '<b>Frame ID:</b> '    + messageData.iframe.id +
+	//     ' <b>Message:</b> '    + messageData.message
+	// );
+	// alert(messageData.message);
     },
     closedCallback         : function(id){ // Callback fn when iFrame is closed
-	console.log(
-	    '<b>IFrame (</b>'    + id +
-	    '<b>) removed from page.</b>'
-	);
+	// console.log(
+	//     '<b>IFrame (</b>'    + id +
+	//     '<b>) removed from page.</b>'
+	// );
     }
 }, '.sakura');
